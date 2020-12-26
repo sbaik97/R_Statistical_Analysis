cc477521122b06acba0e4d25d70b1e1982e71c4c
Sungil Baik <38955694+sbaik97@users.noreply.github.com>
2020-12-25 22:19:09 -0600

    commit on "main"

--- Practice/practice.R
+++ Practice/practice.R
@@ -1,11 +1,37 @@
+library(jsonlite)
+library(tidyverse)
+
 '''{r}
 getwd()
 demo_table <- read.csv(file='../Bootcamp/Week15_R/R_Statistical_Analysis/Practice/Resources/demo.csv',check.names=F,stringsAsFactors = F)
 '''
+demo_table2 <- fromJSON(txt='R_Statistical_Analysis/Practice/Resources/demo.json')
 
 '''{r}
-demo_table2 <- read.csv(file='../Bootcamp/Week15_R/R_Statistical_Analysis/Practice/Resources/demo2.csv',check.names=F,stringsAsFactors = F)
+demo_table3 <- read.csv(file='../Bootcamp/Week15_R/R_Statistical_Analysis/Practice/Resources/demo2.csv',check.names=F,stringsAsFactors = F)
+'''
 
+
+#subset
+filter_table2 <- subset(demo_table2, price > 10000 & drive == "4wd" & "clean" %in% title_status) #filter by price and drivetrain
+
+'''{r}
+# using brackets
+filter_table3 <- demo_table2[("clean" %in% demo_table2$title_status) & (demo_table2$price > 10000) & (demo_table2$drive == "4wd"),]
+'''
+
+'''{r}
+summarize_demo <- demo_table2 %>% group_by(condition) %>%
+  summarize(Mean_Mileage=mean(odometer), .groups = 'keep') #create summary table
+'''
+
+'''{r}
+summarize_demo <- demo_table2 %>% group_by(condition) %>%
+  +     summarize(Mean_Mileage=mean(odometer),Maximum_Price=max(price),Num_Vehicles=n(), .groups = 'keep') #create summary table with multiple columns
+'''
+#long table
+'''{r}
+long_table <- gather(demo_table3,key="Metric",value="Score",buying_price:popularity)
 '''
 
 '''{r}
@@ -19,3 +45,129 @@ mpg_summary <- mpg %>% group_by(manufacturer) %>% summarize(Vehicle_Count=n(), .
 plt <- ggplot(mpg_summary,aes(x=manufacturer,y=Vehicle_Count)) #import dataset into ggplot2
 plt + geom_col() #plot a bar plot
 '''
+
+'''{r}
+#format of plot
+plt + geom_col() + xlab("Manufacturing Company") + ylab("Number of Vehicles in Dataset") + #plot a boxplot with labels
+theme(axis.text.x=element_text(angle=45,hjust=1)) #rotate the x-axis label 45 degrees
+'''
+
+# box plot from mpg data
+'''{r}
+plt <- ggplot(mpg,aes(y=hwy))#import dataset into ggplot2
+plt + geom_boxplot() #add boxplot
+'''
+
+# box plot with category
+'''{r}
+plt <- ggplot(mpg,aes(x=manufacturer,y=hwy)) #import dataset into ggplot2
+plt + geom_boxplot() + theme(axis.text.x=element_text(angle=45,hjust=1)) # 
+'''
+
+# Heatmap of average highway fuel efficiency across the type of vehicle class from 1999 to 2008
+'''{r}
+mpg_summary <- mpg %>% group_by(class,year) %>% summarize(Mean_Hwy=mean(hwy), .groups = 'keep')
+plt <- ggplot(mpg_summary, aes(x=class,y=factor(year),fill=Mean_Hwy))
+plt + geom_tile() + labs(x="Vehicle Class",y="Vehicle Year",fill="Mean Highway (MPG)")
+'''
+#average highway fuel e􀂨ciency across each vehicle model from 1999 to 2008
+'''{r}
+> mpg_summary <- mpg %>% group_by(model,year) %>% summarize(Mean_Hwy=mean(hwy), .groups = 'keep') #create summary table
+> plt <- ggplot(mpg_summary, aes(x=model,y=factor(year),fill=Mean_Hwy)) #import dataset into ggplot2
+> plt + geom_tile() + labs(x="Model",y="Vehicle Year",fill="Mean Highway (MPG)") + #add heatmap with labels > theme(axis.text.x = element_text(angle=90,hjust=1,vjust=.5)) #rotate x-axis labels 90 degrees
+'''
+
+# add additional plots to our visualization by adding additional geom functions
+'''{r}
+> plt <- ggplot(mpg,aes(x=manufacturer,y=hwy)) #import dataset into ggplot2
+> plt + geom_boxplot() + #add boxplot
+> theme(axis.text.x=element_text(angle=45,hjust=1)) + #rotate x-axis labels 45 degrees
+> geom_point() #overlay scatter plot on top
+'''
+
+# mapping argument
+'''{r}
+> mpg_summary <- mpg %>% group_by(class) %>%
+summarize(Mean_Engine=mean(displ), .groups = 'keep') #create summary table
+> plt <- ggplot(mpg_summary,aes(x=class,y=Mean_Engine)) #import dataset into ggplot2
+> plt + geom_point(size=4) + labs(x="Vehicle Class",y="Mean Engine Size") #add scatter plot
+'''
+# mean plus standard deviation
+'''{r}
+> mpg_summary <- mpg %>% group_by(class) %>%
+summarize(Mean_Engine=mean(displ),SD_Engine=sd(displ), .groups = 'keep')
+> plt <- ggplot(mpg_summary,aes(x=class,y=Mean_Engine)) #import dataset into ggplot2
+> plt + geom_point(size=4) + labs(x="Vehicle Class",y="Mean Engine Size") + #add scatter plot with labels
+> geom_errorbar(aes(ymin=Mean_Engine-SD_Engine,ymax=Mean_Engine+SD_Engine)) #overlay with error
+bars
+'''
+#visualize the different vehicle fuel e􀂨ciency ratings by manufacturer
+'''{r}
+> plt <- ggplot(mpg_long,aes(x=manufacturer,y=Rating,color=MPG_Type)) #import
+> plt + geom_boxplot() + theme(axis.text.x=element_text(angle=45,hjust=1)) #
+'''
+
+# visualize the distribution of driven miles for our entire population dataset
+'''{r}
+> population_table <- read.csv(file='R_Statistical_Analysis/Practice/Resources/used_car_data.csv',check.names = F,stringsAsFactors = F) #import used car dataset
+> plt <- ggplot(population_table,aes(x=log10(Miles_Driven))) #import dataset into ggplot2
+> plt + geom_density() #visualize distribution using density plot
+'''
+#two-sample t-Test 
+'''{r}
+> sample_table <- population_table %>% sample_n(50) #generate 50 randomly sam
+> sample_table2 <- population_table %>% sample_n(50) #generate another 50 random
+t.test(log10(sample_table$Miles_Driven),log10(sample_table2$Miles_Driven))
+'''
+# pair t-test
+'''{r}
+> population_table <- read.csv(file='R_Statistical_Analysis/Practice/Resources/mpg_modified.csv',check.names = F,stringsAsFactors = F) #import used car dataset
+> mpg_1999 <- mpg_data %>% filter(year==1999) #select only data points where the year is 1999
+> mpg_2008 <- mpg_data %>% filter(year==2008) #select only data points where the year is 2008
+'''
+# ANOVA Test
+'''{r}
+# the cyl is considered a numerical interval vector, not a categorical vector. 
+# Therefore, we must clean our data before we begin,
+> mtcars_filt <- mtcars[,c("hp","cyl")] #filter columns from mtcars dataset
+> mtcars_filt$cyl <- factor(mtcars_filt$cyl) #convert numeric column to factor
+> aov(hp ~ cyl,data=mtcars_filt) #compare means across multiple levels
+> summary(aov(hp ~ cyl,data=mtcars_filt))
+'''
+
+# pair t-test
+'''{r}
+used_cars <- read.csv(file='R_Statistical_Analysis/Practice/Resources/used_car_data.csv',check.names = F,stringsAsFactors = F)
+head(used_cars)
+cor(used_cars$Miles_Driven,used_cars$Selling_Price) #calculate correlation
+'''
+
+#produce a correlation matrix for our used_cars dataset
+'''{r}
+> used_matrix <- as.matrix(used_cars[,c("Selling_Price","Present_Price","Miles_Driven")])
+#convert data frame into numeric matrix
+> cor(used_matrix)
+'''
+
+# Linear Regression
+'''{r}
+> model <- lm(qsec ~ hp,mtcars) #create linear model
+> yvals <- model$coefficients['hp']*mtcars$hp + model$coefficients['(Intercept)'] #determine y-axis values from linear model
+> plt <- ggplot(mtcars,aes(x=hp,y=qsec)) #import dataset into ggplot2
+> plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter
+'''
+
+# Multiple linear Regression
+'''{r}
+> lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars) #generate multiple line
+> summary(lm(qsec ~ mpg + disp + drat + wt + hp,data=mtcars)) #generate summa
+'''
+
+# chi-squared test
+'''{r}
+#statistical difference in the distributions of vehicle class across 1999 and 2008 from our mpg
+> table(mpg$class,mpg$year) #generate contingency table
+#pass the contingency table to the chisq.test() function:
+> tbl <- table(mpg$class,mpg$year) #generate contingency table
+> chisq.test(tbl) #compare categorical distributions
+
